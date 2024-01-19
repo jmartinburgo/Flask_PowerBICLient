f59bf70bc549697728e11fc51c79e8756c7650f8
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2024-01-19 12:58:53 -0300

    Login Front

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
-from flask import Flask ,render_template
+from flask import Flask , render_template
 from config import config
 
 app= Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('auth/login.html')
 
 if __name__ == '__main__':
-    app.config.from_object(config['development'])   
+    app.config.from_object(config['development'])  
 
     app.run()
