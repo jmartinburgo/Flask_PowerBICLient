c68394ac935f1dcdb4cd5f6c5c319e2133895af5
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2024-01-20 21:01:18 -0300

    Check Hash

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
-class DevelopmentConfig():
+class Config:
+    SECRET_KEY= "asdlkjlkasdjlakssdlkjl"
+
+class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST= 'localhost'
+    MYSQL_USER= 'root'
+    MYSQL_DB= 'flask_login'
 
 
 config = {
