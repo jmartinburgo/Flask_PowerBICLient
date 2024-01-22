3067995210cb75a4512f11228105ca400fb88df5
Juan Martin Burgo Mangiantini <jmartinburgo@hotmail.com>
2024-01-22 15:11:06 -0300

    Login Manager

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 from config import config
 
@@ -13,9 +14,11 @@ app= Flask(__name__)
 
 db = MySQL(app)
 
+login_manager_app= LoginManager(app)
 
-
-
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
 
 @app.route('/')
 def index():
@@ -27,10 +30,11 @@ def login():
         #print(request.form['username'])
         #print(request.form['password'])
         user=User(1,request.form['username'],request.form['password'])
-        logged_user=ModelUser.login(db,user)
+        logged_user=ModelUser.login(db,user)    
         #print(logged_user)
         if logged_user != None:
             if logged_user.password:
+                login_user(logged_user)
                 return redirect(url_for('home'))
             else:
                 flash("Invalid Password...")  
--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -7,12 +7,30 @@ class ModelUser():
         try:
             cursor=db.connection.cursor()
             sql= """ SELECT id, username, password, fullname FROM user 
-                        WHERE username = '{}'""".format(user.username)
+                        WHERE username = {}""".format(user.username)
             cursor.execute(sql)
             row=cursor.fetchone()
             if row != None:
                 user=User(row[0],row[1],User.check_password(row[2],user.password))
-                
+                return user
+            else:
+                return None
+
+        except Exception as ex:
+            raise Exception(ex)
+        
+
+    @classmethod
+    def get_by_id(self, db , id):
+        try:
+            cursor=db.connection.cursor()
+            sql= """ SELECT id, username, fullname FROM user 
+                        WHERE id = {}""".format(id)
+            cursor.execute(sql)
+            row=cursor.fetchone()
+            if row != None:
+                return User(row[0],row[1],None, row[2]) #Se puede hacer asi o definir la variable y despues retornar como el caso anterior
+                 
             else:
                 return None
 
